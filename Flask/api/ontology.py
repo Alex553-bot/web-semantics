9dbc0b5cead59a4478baa68913714c085a99f7bc
alexalvarez <alvarez.rojas.alexander.james5@gmail.com>
2024-12-09 20:26:23 -0400

    add: fuzzy match + nlp + smart search engine based on nlp

--- Flask/api/ontology.py
+++ Flask/api/ontology.py
@@ -1,33 +1,35 @@
 from owlready2 import *
 from pathlib import Path
+from rapidfuzz import fuzz
+
 from restructure import *
+from preprocess import preprocess
 
 path = Path(__file__).parent.resolve()
 path = path.parent.parent
-path = path/"oncology.rdf"
+print(path)
+path = path/"resourse/ontology.owx"
 
 ontologie = get_ontology(str(path)).load()
 # get_ontology("C:/Users/USER/Documents/WebSemantica/web-semantics/oncology.rdf").load()
 
-def search(queries): 
+def search(query):
+	query = preprocess(query)
 	results = {}
-	for class_ in ontologie.classes():
-		for individual in class_.instances():
-			for propertie in individual.get_properties():
-				for query in queries:
-					if contains(query, getattr(individual, propertie.name)):
-						if class_.name not in results:
-							results[class_.name] = []
-						results[class_.name].append({'name':individual.name, 'iri': (individual.iri)})
+	for individual in ontologie.individuals():
+		#print(f"Searching within : {individual}")
+		for propertie in individual.get_properties(): 
+			for value in getattr(individual, propertie.name, None): 
+				match = fuzzy_match(preprocess(str(value)), query)
+				if match>=40.0: 
+					class_name = str(list(individual.is_a)[0])
+					if class_name not in results: 
+						results[class_name] = []
+					results[class_name].append({'name': individual.name, 'iri': individual.iri})
 	return results
 
-def contains(element_:str, list_): 
-    for element in list_: 
-        element = str(element)
-        x = element.find(element_)
-        if x!=-1: 
-            return True
-    return False
+def fuzzy_match(s, t): 
+	return fuzz.partial_ratio(s, t)
 
 def get(iri): 
 	return ontologie[iri[iri.find('#'):]] # aqui deberia estar el cuerpo completo de un item basado en su iri
@@ -56,28 +58,3 @@ def getClassesOntologie():
 				"sub_clasess": struct_class(classOntology),
 			})
 	return clasess
-
-print(ontologie.base_iri)
-
-from rdflib import Graph, Namespace #, URIRef, Literal
-g = Graph()
-g.parse(str(path), format='xml')
-ns = Namespace(ontologie.base_iri)
-query = """
-PREFIX : <http://www.semanticweb.org/ontologies/oncology-ontology/>
-
-SELECT  ?Cancer
-WHERE {
-  ?Cancer a :Cancer .
-  ?Cancer :nombre "Cancer de pancreas" .
-}
-"""
-
-results = g.query(query)
-for row in results: 
-	print(row)
-
-# Prueba de instancias de clases
-	
-
-# print(f"Instancias de la clase Cancer: {getInstancesByClass("Cancer")}")

15aee4c1d05c04f1f9f652f9ecc8c054858f70a6
William Calle Pinto <cwilliam14c@gmail.com>
2024-12-11 01:35:20 -0400

    pre internacionalizacion

--- Frontend/src/components/Search.jsx
+++ Frontend/src/components/Search.jsx
@@ -1,8 +1,19 @@
 import { useState } from 'react';
-import { Button, Form, InputGroup } from 'react-bootstrap';
+import { Button, Col, Form, InputGroup, Row } from 'react-bootstrap';
 import Result from './Result';
+import messages_es from '../translations/es.json';
+import messages_en from '../translations/en.json';
+import { FormattedMessage, IntlProvider } from 'react-intl';
+
+const messages = {
+  es: messages_es,
+  en: messages_en,
+}
+
+const translations = ['es', 'en'];
 
 function Search() {
+  const [locale, setLocale] = useState('es');
   const [query, setQuery] = useState(null);
   const [result, setResult] = useState(null);
   
@@ -17,33 +28,54 @@ function Search() {
       .catch(error => console.error(error));
   }
 
+  const handleChangeTranslation = (e) => {
+    e.preventDefault();
+    setLocale(e.target.value);
+  }
+
   return (
     <div>
-      <Form onSubmit={handleSubmit}>
-        <div className='mt-3 mb-2 mx-auto' style={{ width: '50%' }}>
-          <InputGroup size='lg'>
-            <Form.Control
-              placeholder='Escriba su búsqueda'
-              onChange={(e) => setQuery(e.target.value) }
-            />
-            <Button
-              variant='outline-secondary'
-              id='search-button'
-              type='submit'
-            >
-              Buscar
-            </Button>
-          </InputGroup>
-        </div>
-      </Form>
+      <IntlProvider locale={locale} messages={messages[locale]}>
+        <Row className='align-items-center'>
+          <Col xs={9}>
+            <Form onSubmit={handleSubmit}>
+              <div className='mt-3 mb-2 mx-auto' style={{ width: '70%' }}>
+                <InputGroup size='lg'>
+                  <Form.Control
+                    placeholder={messages[locale]['app.placeholder']}
+                    onChange={(e) => setQuery(e.target.value) }
+                  />
+                  <Button
+                    variant='outline-secondary'
+                    id='search-button'
+                    type='submit'
+                  >
+                    <FormattedMessage id='app.search-button' />
+                  </Button>
+                </InputGroup>
+              </div>
+            </Form>
+          </Col>
 
-      <div className='mx-auto' style={{ width: '80%' }}>
-        {result && Object.keys(result).map(k => (
-          <div className='m-3'>
-            <Result nameClass={k} arrayClass={result[k]} />
-          </div>
-        ))}
-      </div>
+          <Col xs={2}>
+            <Form.Select onChange={handleChangeTranslation}>
+              {translations.map(t => (
+                <option value={t}>
+                  <FormattedMessage id={t} />
+                </option>
+              ))}
+            </Form.Select>
+          </Col>
+        </Row>
+
+        <div className='mx-auto' style={{ width: '80%' }}>
+          {result && Object.keys(result).map(k => (
+            <div className='m-3'>
+              <Result nameClass={k} arrayClass={result[k]} />
+            </div>
+          ))}
+        </div>
+      </IntlProvider>
     </div>
   );
 }
